1c80f76f94f542c83fe8f62e4abef64e16cbcd75
DRAGONINWAVE <yl597591@gmail.com>
2022-09-15 20:55:43 +0800

    asyncio

---
+++ Chapter2_asyncio_basics/Creating_a_task.py
@@ -0,0 +1,10 @@
+import asyncio
+from A_reusable_delay_function import delay
+
+async def main():
+    sleep_for_three = asyncio.create_task(delay(3))
+    print(type(sleep_for_three))
+    result = await sleep_for_three
+    print(result)
+
+asyncio.run(main())
