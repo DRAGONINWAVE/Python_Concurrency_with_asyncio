941493c55de232b4832ace69b5debc9a2148abe3
DRAGONINWAVE <yl597591@gmail.com>
2022-09-17 20:14:08 +0800

    呜呜呜呜

--- Chapter3_A_first_asyncio_application/Starting_a_server_and_listening_for_a_connection.py
+++ Chapter3_A_first_asyncio_application/Starting_a_server_and_listening_for_a_connection.py
@@ -1,4 +1,12 @@
 from http import server
 import socket
 
-server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_address = ('127.0.0.1', 8000)
+server_socket.bind(server_address)
+server_socket.listen()
+
+connection, client_address = server_socket.accept()
+print(f'I got a connection from {client_address} !')
