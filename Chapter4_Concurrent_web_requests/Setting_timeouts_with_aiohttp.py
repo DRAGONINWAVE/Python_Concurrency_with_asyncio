dc2925e5e59f499558b44ccde51d747131c7993e
DRAGONINWAVE <yl597591@gmail.com>
2022-09-20 22:12:31 +0800

    await

---
+++ Chapter4_Concurrent_web_requests/Setting_timeouts_with_aiohttp.py
@@ -0,0 +1,18 @@
+import asyncio
+import aiohttp
+from aiohttp import ClientSession
+
+
+async def fetch_status(session: ClientSession,
+                       url: str) -> int:
+    ten_millis = aiohttp.ClientTimeout(total=.01)
+    async with session.get(url, timeout=ten_millis) as result:
+        return result.status
+
+
+async def main():
+    sesseion_timeout = aiohttp.ClientTimeout(total=1, connect=.1)
+    async with aiohttp.ClientSession(timeout=sesseion_timeout) as session:
+        await fetch_status(session, 'https://example.com')
+
+asyncio.run(main())
