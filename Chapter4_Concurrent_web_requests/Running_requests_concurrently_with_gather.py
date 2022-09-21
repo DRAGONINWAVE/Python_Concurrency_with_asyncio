a11cb26b372938317279b8fe009a18bccc6c25c0
DRAGONINWAVE <yl597591@gmail.com>
2022-09-21 08:47:06 +0800

    status_fetch

---
+++ Chapter4_Concurrent_web_requests/Running_requests_concurrently_with_gather.py
@@ -0,0 +1,16 @@
+import asyncio
+import aiohttp
+from aiohttp import ClientSession
+from A_decorator_for_timing_coroutine import async_timed
+from Making_an_atihttp_web_request import fetch_status
+
+
+@async_timed()
+async def main():
+    async with aiohttp.ClientSession() as session:
+        urls = ['https://example.com' for _ in range(10000)]
+        requests = [fetch_status(session, url) for url in urls]
+        status_code = await asyncio.gether(*requests)
+        print(status_code)
+
+asyncio.run(main())
