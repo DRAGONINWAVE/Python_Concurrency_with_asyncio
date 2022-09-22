dc24dd79954654f380070f91838c8966c1944933
DRAGONINWAVE <yl597591@gmail.com>
2022-09-22 21:15:45 +0800

    jingjie!!

---
+++ Chapter4_Concurrent_web_requests/Processing_as_the_complete.py
@@ -0,0 +1,25 @@
+from sys import exc_info
+import aiohttp
+import asyncio
+import logging
+from Using_as_completed import fetch_status
+from A_decorator_for_timing_coroutine import async_timed
+
+
+@async_timed()
+async def main():
+    async with aiohttp.ClientSession() as session:
+        url = 'https://www.example.com'
+        fetchers = [asyncio.create_task(fetch_status(session, url)),
+                    asyncio.create_task(fetch_status(session, url)),
+                    asyncio.create_task(fetch_status(session, url))]
+
+        done, pending = await asyncio.wait(fetchers, return_when=asyncio.FIRST_COMPLETED)
+
+    print(f'Done task count:{len(done)}')
+    print(f'Pending task count:{len(pending)}')
+
+    for done_task in done:
+        print(await done_task)
+
+asyncio.run(main())
---
+++ help_others/xiawei/dropnun.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_excel(r'D:\\piles\\20220617.xlsx')
+print(data)
